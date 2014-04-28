e514746d3a0cd7785e4ae0982a27710848cacc66
Charles Chu <cchu@opentable.com>
2014-04-28 14:36:31 -0700

    Update getWindow and getNavigator function

--- public/javascripts/GetDeviceInfo.js
+++ public/javascripts/GetDeviceInfo.js
@@ -1,10 +1,10 @@
-var getNavigator = (function() {
+var getNavigator = function() {
     return navigator;
-})();
+};
 
-var getWindow = (function() {
-    return $(window);
-})();
+var getWindow = function() {
+    return window;
+};
 
 var DeviceType = {
     phone: "phone",
@@ -15,64 +15,44 @@ var DeviceType = {
 };
 
 var getPageSize = function () {
-  var $window = window,
-      $document = $window.document;
+    var $window = $(getWindow());
 
-  var width = Math.max(
-        $document.body.scrollWidth, $document.documentElement.scrollWidth,
-        $document.body.offsetWidth, $document.documentElement.offsetWidth,
-        $document.body.clientWidth, $document.documentElement.clientWidth
-    );
-
-  var height = Math.max(
-        $document.body.scrollHeight, $document.documentElement.scrollHeight,
-        $document.body.offsetHeight, $document.documentElement.offsetHeight,
-        $document.body.clientHeight, $document.documentElement.clientHeight
-    );
-
-  return {
-      width: width,
-      height: height,
-      screenWidth: $window.screen.width,
-      screenHeight: $window.screen.height
-  };
+    return {
+        width: $window.width(),
+        height: $window.height(),
+        screenWidth: window.screen.width,
+        screenHeight: window.screen.height
+    };
 };
 
-var getDeviceInfo = function ($pageSize) {
+var getDeviceInfo = function (pageSize) {
     'use strict';
 
-    var $window = window;
-    var isPortrait = ($pageSize.width < $pageSize.height);
-    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test($window.navigator.userAgent);
+ var $navigator = getNavigator();
+    var isPortrait = (pageSize.width < pageSize.height);
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test($navigator.userAgent);
 
-    var width = isMobile ? $pageSize.screenWidth : $pageSize.width;
-    var height = isMobile ? $pageSize.screenHeight : $pageSize.height;
+    var width = isMobile ? pageSize.screenWidth : pageSize.width;
+    var height = isMobile ? pageSize.screenHeight : pageSize.height;
 
-    if (width <= 0 || height <= 0) {
-        width = 0;
-        height = 0;
-    }
+    var getDeviceType = function () {
 
-  var getDeviceType = function () {
+        var maxWidth = (width <= 0 || height <= 0) ? 0 : Math.max(width, height);
 
-    var maxWidth = Math.max(width, height);
-
-    if (maxWidth > 0 && maxWidth < 641) {
-      return DeviceType.phone;
-    } else if (maxWidth > 640 && maxWidth < 1025) {
-      return DeviceType.tablet;
-    } else if (maxWidth > 1440 && maxWidth < 1921) {
-      return DeviceType.xLarge;
-    } else if (maxWidth > 1920) {
-      return DeviceType.xXLarge;
-    } else {
-      return DeviceType.large;
-    }
+        if (maxWidth > 0 && maxWidth < 641) {
+            return DeviceType.phone;
+        } else if (maxWidth > 640 && maxWidth < 1025) {
+            return DeviceType.tablet;
+        } else if (maxWidth > 1440 && maxWidth < 1921) {
+            return DeviceType.xLarge;
+        } else if (maxWidth > 1920) {
+            return DeviceType.xXLarge;
+        } else {
+            return DeviceType.large;
+        }
     };
 
     return {
-        width: width,
-        height: height,
         deviceType: getDeviceType(),
         isPortrait: isPortrait,
         isMobile: isMobile
